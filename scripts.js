a7e9aeb340dca16b69ac4c7e27bd5953910df68b
DanielHaigh3000 <azrael3000@gmail.com>
2024-02-18 20:23:41 -0400

    Added scrollbar, fixed extra decimal periods

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 let total;
 let curNum;
 let lastCalc;
+let hasDecimal = false;
 
 // Select elements
 const calculatorViewport = document.querySelector('.calculator__viewport');
@@ -42,9 +43,20 @@ const getCurrentNumber = function (e) {
 
 // Display currently pressed number on screen
 const displayNum = function (e) {
+  // Double-checking that we're getting a number
   if (e.target.dataset.hasOwnProperty('num')) {
+    // If getting decimal AND we already have one, stop function to prevent double decimal in display
+    if (e.target.dataset.num === '.' && hasDecimal) return;
+
+    // If getting decimal and we don't already have one ...
+    // Change flag so we know to ignore next decimal
+    if (e.target.dataset.num === '.') hasDecimal = true;
+
+    // Otherwise, display the decimal and keep moving along
     calculatorInput.innerText += e.target.dataset.num;
     calculatorTempTotal.innerText += e.target.dataset.num;
+    // Hor scroll anchored right with long nums
+    calculatorInput.scrollTo(calculatorInput.offsetWidth, 0);
   }
 };
 
@@ -61,6 +73,7 @@ const checkCalculation = function (e, calcType) {
     displayCalc(e);
     resetTempTotal();
     total = curNum;
+    curNum = undefined; // Protects against someone doing ex. 2 + = (forces second number)
     return total;
   }
 
@@ -82,6 +95,7 @@ const resetTempTotal = function () {
 const displayCalc = function (e) {
   if (e.target.dataset.hasOwnProperty('key')) {
     calculatorInput.innerText += e.target.dataset.key;
+    hasDecimal = false;
   }
 };
 
@@ -122,6 +136,7 @@ calculatorKeypad
 calculatorKeypad
   .querySelector('[data-key="="')
   .addEventListener('click', () => {
+    if (curNum === undefined || total === undefined) return;
     if (lastCalc === 'add') {
       calcAdd(total, curNum);
     }
@@ -150,6 +165,9 @@ calculatorKeypad
 // ********************* TODO ***********************
 
 // v1. use buttons on the screen
-// v2. recognize keyboard codes as well for addition, multiplication, etc.
+// v2. recognize keyboard codes as well for numbers, addition, multiplication, etc.
 // v3. show X number of previous steps (calculation history)
-// v4. JavaScript decimal number fixing (e.g. 0.1 + 0.2 does NOT equal 0.3)
+
+// Things to fix:
+// a. JavaScript decimal number fixing (e.g. 0.1 + 0.2 does NOT equal 0.3)
+//
